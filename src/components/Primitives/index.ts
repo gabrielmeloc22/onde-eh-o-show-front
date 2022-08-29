d8b38b691c845ae0b8103379cc672d9fac047920
gabrielmeloc22 <gabrielpresb@gmail.com>
2022-08-29 19:16:00 -0300

    fix(primitives): add missing import and export

--- src/components/Primitives/index.ts
+++ src/components/Primitives/index.ts
@@ -1,6 +1,7 @@
 import { Box } from "./Box";
-import { Heading } from "./Heading";
 import { Button } from "./Button";
+import { Heading } from "./Heading";
+import { Link } from "./Link";
 import { Text } from "./Text";
 
-export { Box, Heading, Button, Text };
+export { Box, Heading, Button, Text, Link };
