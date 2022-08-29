8e4c49c4c1c2f631eb32197cf2c24c399d1602eb
gabrielmeloc22 <gabrielpresb@gmail.com>
2022-08-29 19:18:15 -0300

    feat(linkPrimitive): add variants

--- src/components/Primitives/Link.ts
+++ src/components/Primitives/Link.ts
@@ -1,10 +1,27 @@
 import { styled } from "@stitches/react";
 
-
-export const Link = styled('a', {
-  color: '$primary9',
-  '&:hover': {
-    color: '$primary8',
-    textDecoration: 'underline',
-  }
-})
+export const Link = styled("a", {
+  textDecoration: "none",
+  "&:hover": {
+    textDecoration: "underline",
+  },
+  variants: {
+    color: {
+      primary: {
+        color: "$primary10",
+        "&:hover": {
+          color: "$primary11",
+        },
+      },
+      neutral: {
+        color: "$slate11",
+        "&:hover": {
+          color: "$slate11",
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    color: "primary",
+  },
+});
