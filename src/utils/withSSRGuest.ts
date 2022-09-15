ef409b6ca01e39694bfea49eea6b035e129f776d
gabrielmeloc22 <gabrielpresb@gmail.com>
2022-09-15 15:49:24 -0300

    fix: use refresh token to check auth

--- src/utils/withSSRGuest.ts
+++ src/utils/withSSRGuest.ts
@@ -1,17 +1,11 @@
-import {
-  GetServerSideProps,
-  GetServerSidePropsContext,
-  GetServerSidePropsResult,
-} from "next";
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { parseCookies } from "nookies";
 
 export function withSSRGuest<P>(fn: GetServerSideProps<P>) {
-  return async (
-    ctx: GetServerSidePropsContext
-  ): Promise<GetServerSidePropsResult<P>> => {
+  return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
 
-    if (cookies["spotify.access_token"]) {
+    if (cookies["spotify.refresh_token"]) {
       return {
         redirect: {
           destination: "/top-artists",
