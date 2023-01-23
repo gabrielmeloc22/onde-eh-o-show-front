b3c4c1f48c9113ac94d5c486c8f6ed75c2c3d34c
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-01-23 13:53:45 -0300

    refactor(ArtistCard): add animatePresence

--- src/components/ArtistCard/index.tsx
+++ src/components/ArtistCard/index.tsx
@@ -1,4 +1,6 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import { styled } from "../../styles/stitches.config";
 import { Box, Text } from "../Primitives";
 import { Skeleton } from "../Skeleton";
 import { ArtistImage } from "./ArtistImage";
@@ -10,46 +12,63 @@ interface ArtistCardProps {
   id: string;
 }
 
+const Wrapper = styled(motion.div, {
+  position: "relative",
+  display: "flex",
+  flexDir: "column",
+  bgColor: "$slate4",
+  gap: "$4",
+  maxH: "fit-content",
+  zIndex: 0,
+});
+
 export function ArtistCard({ id }: ArtistCardProps) {
   const { data: artist, isFetching: isFetchingArtist } = useGetArtistQuery(id);
   const [hover, setHover] = useState(false);
 
   return (
-    <Box
-      css={{
-        position: "relative",
-        display: "flex",
-        flexDir: "column",
-        bgColor: "$slate4",
-        gap: "$4",
-        maxH: "fit-content",
-        zIndex: 0,
-      }}
-      tabIndex={0}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      onFocus={() => setHover(true)}
-      onBlur={() => setHover(false)}
-    >
-      <CardOptions artistId={id} hover={hover} />
-      <ArtistImage alt={artist?.name ?? ""} image={artist?.image} />
-      <Box
-        css={{
-          h: "100%",
-          display: "flex",
-          flexDir: "column",
-          gap: "$4",
-          justifyContent: "space-between",
-          p: "$4",
-        }}
-      >
-        <Skeleton isLoaded={!isFetchingArtist}>
-          <Text weight="bold" size="large">
-            {artist?.name}
-          </Text>
-        </Skeleton>
-        <EventsInfo artistId={id} />
-      </Box>
-    </Box>
+    <AnimatePresence>
+      {artist && (
+        <Wrapper
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0 }}
+          css={{
+            position: "relative",
+            display: "flex",
+            flexDir: "column",
+            bgColor: "$slate4",
+            gap: "$4",
+            maxH: "fit-content",
+            zIndex: 0,
+          }}
+          tabIndex={0}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          onFocus={() => setHover(true)}
+          onBlur={() => setHover(false)}
+        >
+          <CardOptions artistId={id} hover={hover} />
+          <ArtistImage alt={artist?.name ?? ""} image={artist?.image} />
+          <Box
+            css={{
+              h: "100%",
+              display: "flex",
+              flexDir: "column",
+              gap: "$4",
+              justifyContent: "space-between",
+              p: "$4",
+            }}
+          >
+            <Skeleton isLoaded={!isFetchingArtist}>
+              <Text weight="bold" size="large">
+                {artist?.name}
+              </Text>
+            </Skeleton>
+            <EventsInfo artistId={id} />
+          </Box>
+        </Wrapper>
+      )}
+    </AnimatePresence>
   );
 }
